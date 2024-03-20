e3745c15b3a5b90abacc125e1a314101163ae1d6
Anone-75 <153702776+Anone-75@users.noreply.github.com>
2024-03-20 23:22:25 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,34 +1,30 @@
-class Search {
-    public static int linearSearch(int[] arr, int target) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == target) {
-                return i;
-            }
-        }
-        return -1;
-    }
+public class BinarySearch {
+    public static int binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
 
-    public static int binarySearch(int[] arr, int target) {
-        int low = 0, high = arr.length - 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (arr[mid] == target) {
-                return mid;
-            } else if (arr[mid] < target) {
-                low = mid + 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (array[mid] == target) {
+                return mid; // Return the index if the target is found
+            } else if (array[mid] < target) {
+                left = mid + 1;
             } else {
-                high = mid - 1;
+                right = mid - 1;
             }
         }
-        return -1;
+        return -1; // Return -1 if the target is not found in the array
     }
-}
 
-public class BinarySearch{
     public static void main(String[] args) {
-        int[] arr = {1, 3, 5, 7, 9, 11, 13, 15};
+        int[] array = {2, 5, 7, 9, 11, 13, 15, 17};
 
-        System.out.println(Search.linearSearch(arr, 7));  // Output: 3
-        System.out.println(Search.binarySearch(arr, 11)); // Output: 5
+        int target = 11;
+        int index = binarySearch(array, target);
+        if (index != -1) {
+            System.out.println("Element " + target + " found at index " + index);
+        } else {
+            System.out.println("Element " + target + " not found in the array");
+        }
     }
 }
