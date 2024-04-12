b2ac7055ef63654a6bc50ac16951a22c25603c3e
Anone-75 <153702776+Anone-75@users.noreply.github.com>
2024-04-12 08:42:16 +0530

    Update StackDemo.java

--- StackDemo.java
+++ StackDemo.java
@@ -1,44 +1,59 @@
 import java.util.*;
 
-public class StackDemo{
-    private int maxsize,top;
-    private int [] stackArray;
+public class StackDemo {
+    private int maxsize, top;
+    private int[] stackArray;
 
-    public StackDemo(int size){
-        this.maxsize=size;
+    public StackDemo(int size) {
+        this.maxsize = size;
         this.stackArray = new int[maxsize];
-        this.top=-1;
+        this.top = -1;
     }
-    public void print(){
-        for (int i=0; i<=top;i++){
-             System.out.println(stackArray[i]);        
-            }
-       
-    }
-    public void push(int value){
 
+    public void print() {
+        for (int i = 0; i <= top; i++) {
+            System.out.println(stackArray[i]);
+        }
+    }
+
+    public void push(int value) {
         stackArray[++top] = value;
-        System.out.println(value+"inserted");
-        return;
+        System.out.println(value + " inserted");
     }
-    public int pop(){
-        return stackArray[top --];
+
+    public int pop() {
+        return stackArray[top--];
     }
-    public boolean isEmpty(){
-        return (top== -1);
+
+    public int peek() {
+        if (top == -1) {
+            System.out.println("Stack is empty");
+            return -1; // or throw an exception
+        }
+        return stackArray[top];
     }
+
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a Size for stack");
+        System.out.println("Enter the size for the stack:");
         int s = sc.nextInt();
         StackDemo st = new StackDemo(s);
 
-       for (int i=0;i<s;i++){
-        st.push(sc.nextInt());
-       }
-        System.out.println(st.isEmpty());
-        System.out.println("Pop element :"+st.pop());
+        System.out.println("Enter elements to push into the stack:");
+        for (int i = 0; i < s; i++) {
+            st.push(sc.nextInt());
+        }
+
+        System.out.println("Is stack empty? " + st.isEmpty());
+        System.out.println("Top element (peek): " + st.peek());
+        System.out.println("Pop element: " + st.pop());
+        System.out.println("After popping, elements in the stack:");
         st.print();
+
         sc.close();
     }
 }
